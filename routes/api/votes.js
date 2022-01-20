92cfa5a74750d6cd9a8864b574fcf553b1aa632c
James Tuttle <jaymz69@yahoo.com>
2022-01-20 15:05:19 -0800

    createing api for votes.

--- routes/api/votes.js
+++ routes/api/votes.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const db = require('../db/models');
-const { asyncHandler } = require('../utils.js');
+const db = require('../../db/models');
+const { asyncHandler } = require('../../utils.js');
 
 const router = express.Router();
 
-router.get('/votes/:id(\\d+)', asyncHandler(async (req, res) => {
+// router.get('/votes/:id(\\d+)', asyncHandler(async (req, res) => {
 
-}))
+
+//     console.log(votes);
+// }));
 
 router.post('answers/:answerId(\\d+)/votes', asyncHandler(async (req, res) => {
-    
-}))
+
+}));
 
 router.put('/votes/:id(\\d+)', asyncHandler(async (req, res) => {
-    
-}))
+    const { id } = req.params;
+    console.log(id);
+}));
 
 router.delete('/votes/:id(\\d+)', asyncHandler(async (req, res) => {
-    
-}))
+
+}));
 
 module.exports = router;
