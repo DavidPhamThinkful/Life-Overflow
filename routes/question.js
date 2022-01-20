84ec3918c5bbb823db9228529574814fb3b9feb4
Casey Spears <spearscase@gmail.com>
2022-01-19 17:57:39 -0700

    rendering edit and delete for answers

--- routes/question.js
+++ routes/question.js
@@ -8,7 +8,6 @@ const router = express.Router();
 router.get('/', asyncHandler((async (req, res) => {
     const questions = await db.Question.findAll({
         include: [{model: db.Answer}, {model: db.Category}, {model: db.User}]});
-        console.log(questions)
         res.render('question', {title:'Here is the list of questions', questions, user: res.locals.user});
 })));
 
@@ -52,8 +51,8 @@ router.post('/',requireAuth, csrfProtection, asyncHandler((async (req, res) => {
 router.get('/:id(\\d+)',requireAuth, asyncHandler((async (req, res) => {
     const questions = [await db.Question.findOne({ where: {id: req.params.id},
         include: [{model: db.Answer}, {model: db.Category}, {model: db.User}]})];
-        console.log(questions)
-        res.render('question', {title: 'Answer and Begone', questions, user: res.locals.user});
+        console.log(questions[0].Answers[0])
+        res.render('question', {title: 'Answer and Begone', questions, id: req.params.id, user: res.locals.user});
 })));
 
 router.get('/:id/edit',requireAuth, asyncHandler((async (req, res) =>{
