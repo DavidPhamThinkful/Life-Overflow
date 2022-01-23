0b5a71eee9f9c93d8fef466cb392e0e608c19a01
Casey Spears <spearscase@gmail.com>
2022-01-23 11:31:56 -0700

    testing for final

--- db/seeders/20220119171231-seedData.js
+++ db/seeders/20220119171231-seedData.js
@@ -10,8 +10,8 @@ module.exports = {
       Example:
       */
         const users = await queryInterface.bulkInsert('Users', [
-            { username: 'Demo', email: 'demo@test.com', hashedPassword: await bcrypt.hash('password', 10) },
-            { username: 'Joe', email: 'joe@test.com', hashedPassword: await bcrypt.hash('password', 10) },
+            { username: 'Woman1984', email: 'w1984@w1984.com', hashedPassword: await bcrypt.hash('password', 10) },
+            { username: 'JusticeCouple', email: 'hotjl@hotjl.com', hashedPassword: await bcrypt.hash('password', 10) },
         ], { fields: ['username', 'email', 'hashedPassword'], returning: true });
 
         const categories = await queryInterface.bulkInsert('Categories', [
@@ -23,26 +23,24 @@ module.exports = {
         ], { fields: ['name'], returning: true });
 
         const questions = await queryInterface.bulkInsert('Questions', [
-            { title: 'Question 1', body: 'Question 1 body', userId: users[0].id, categoryId: categories[0].id },
-            { title: 'Question 2', body: 'Question 2 body', userId: users[1].id, categoryId: categories[2].id },
+            { title: 'My Sweet Snowball', body: 'I lost my Snowball. How do I continue without her?', userId: 1, categoryId: 5},
+            { title: 'Congratulations', body: 'My best friend just had his first child. Should we get something for him or for his baby?', userId: 2, categoryId: 2},
+            { title: 'Blood is thicker than water?!?!', body: 'Is it wrong that we feel like our friends are more family than our actual family?', userId: 2, categoryId: 1},
+            { title: 'Cat-Hater-Cure...', body: 'How can I get my new girlfriend to accept my 8 cats?', userId: 1, categoryId: 3},
         ], { fields: ['title', 'body', 'userId', 'categoryId'], returning: true });
 
         const answers = await queryInterface.bulkInsert('Answers', [
-            { body: 'Question 1 answer', userId: users[0].id, questionId: questions[0].id },
-            { body: 'Question 1 answer2', userId: users[1].id, questionId: questions[0].id },
-            { body: 'Question 2 answer', userId: users[0].id, questionId: questions[1].id },
-            { body: 'Question 2 answer2', userId: users[1].id, questionId: questions[1].id },
+            { body: 'My husband and I had out Lassie creamated. She now watches over us from our mantle.', userId: 2, questionId: 1},
+            { body: 'A new cat is always a wonderful surprise on any occasion!', userId: 1, questionId: 2},
+            { body: 'NOT AT ALL! My cats are closer to me now than my family.', userId: 1, questionId: 3},
+            { body: 'Dude, it might just be that eight cats is just eight too many...', userId: 2, questionId: 4},
         ], { fields: ['body', 'userId', 'questionId'], returning: true });
 
         await queryInterface.bulkInsert('Votes', [
-            { value: true, userId: users[0].id, answerId: answers[0].id },
-            { value: true, userId: users[1].id, answerId: answers[0].id },
-            { value: true, userId: users[0].id, answerId: answers[1].id },
-            { value: false, userId: users[1].id, answerId: answers[1].id },
-            { value: true, userId: users[0].id, answerId: answers[2].id },
-            { value: false, userId: users[1].id, answerId: answers[2].id },
-            { value: false, userId: users[0].id, answerId: answers[3].id },
-            { value: false, userId: users[1].id, answerId: answers[3].id },
+            { value: true, userId: 1, answerId: 1},
+            { value: false, userId: 2, answerId: 2},
+            { value: false, userId: 2, answerId: 3},
+            { value: false, userId: 1, answerId: 4},
         ], { fields: [ 'value', 'userId', 'questionId'] });
     },
 
