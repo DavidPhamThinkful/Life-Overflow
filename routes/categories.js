c1c6500485ffe79a837b5cfc5081e738732e948d
James <jaymz69@yahoo.com>
2022-04-18 09:52:50 -0700

    added the a link so can hover category image and click it too to get to Q&A.

--- routes/categories.js
+++ routes/categories.js
@@ -8,8 +8,8 @@ const images = {
     family: 'https://compass-ssl.microsoft.com/assets/fe/5b/fe5b766b-2852-4866-8dcb-fc93763fad6e.png?n=FamilyHeroAnim2_poster_trimmed.png',
     friends: 'https://clipartix.com/wp-content/uploads/2018/01/Friends-clipart-free-download-clip-art-on.jpg',
     romance: 'https://www.pngkit.com/png/detail/171-1719776_romance-clipart-romantic-clipart.png',
-    life: 'https://www.pngall.com/wp-content/uploads/1/Life-PNG-Clipart.png',
-    death: 'https://www.pikpng.com/pngl/b/278-2786753_jpg-freeuse-stock-death-pass-away-frames-illustrations.png',
+    life: 'http://drvidyahattangadi.com/wp-content/uploads/2016/02/Spirit1.jpg',
+    death: 'https://www.clipartkey.com/mpngs/m/172-1721967_death-clipart-deceased-cartoon.png',
 };
 
 router.get('/:id', asyncHandler(async (req, res) => {
